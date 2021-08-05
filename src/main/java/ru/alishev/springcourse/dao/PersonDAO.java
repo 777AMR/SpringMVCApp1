840b4083c40f6a1fe5022ed8f495368cd4d2f863
777AMR <meyram.ramazanov@mail.ru>
2021-08-05 17:11:27 +0600

    Lesson 22. Аннотация @ModelAttribute. HTML Формы (Thymeleaf).

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -8,23 +8,28 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static  int PEOPLE_COUNT;
+    private static int PEOPLE_COUNT;
     private List<Person> people;
 
     {
         people = new ArrayList<>();
 
         people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Jim"));
-        people.add(new Person(++PEOPLE_COUNT, "Alex"));
-        people.add(new Person(++PEOPLE_COUNT, "George"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
 
-    public Person show(int id){
+    public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
